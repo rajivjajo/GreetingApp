cc9b3e98ae07e1b13e57c0e22b4777a406394ef7
rajivjajo <rajivjajodia82552@gmail.com>
2023-04-08 20:42:16 +0530

    Added: greeting with service layer added.

--- src/main/java/com/bridgelabz/greeting/controller/GreetingController.java
+++ src/main/java/com/bridgelabz/greeting/controller/GreetingController.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.greeting.controller;
-
 import com.bridgelabz.greeting.model.Greeting;
+import com.bridgelabz.greeting.service.GreetingService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +14,15 @@ import java.util.concurrent.atomic.AtomicLong;
 public class GreetingController {
     public static final String template = "Hello, %s!";
     public final AtomicLong counter = new AtomicLong();
+    @Autowired
+    public GreetingService service;
 
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+    @RequestMapping("/welcome")
+    public String displayMessage(){
+        return service.greetingMessage();
     }
 }
